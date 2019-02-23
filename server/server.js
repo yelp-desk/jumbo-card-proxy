98e47839011d4a2bc1392eef758deeb241fe3637
Melsm650 <wp.developerhelpers@gmail.com>
2019-02-22 19:41:31 -0800

    EBSProxy Update with live components on AWS

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.static((__dirname + '/../public/')));
 
+app.use('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/../public/'));
+});
 app.listen(port, () => {
 	console.log(`server running at: http://localhost:${port}`);
 });
